192992825f4e259f6a45954837c8e351b653e9b3
J0ye <jonathan.mueller@fh-erfurt.de>
2021-05-03 13:00:32 +0200

    Added comment to programm.cs

--- UnityWebsocketServer/Programm.cs
+++ UnityWebsocketServer/Programm.cs
@@ -59,3 +59,26 @@ namespace Example
         }
     }
 }
+
+/*
+https://github.com/sta/websocket-sharp/issues/356 
+I don't know if this will help in practice or not, but you should check WebSocket.SslConfiguration.EnabledSslProtocols. This resolves as type System.Security.Authentication.SslProtocols which is an enum:
+
+[Flags]
+public enum SslProtocols
+{
+    None = 0,
+    Ssl2 = 12,
+    Ssl3 = 48,
+    Tls = 192,
+    Default = 240,
+    Tls11 = 768,
+    Tls12 = 3072
+}
+If you examine the documentation for Default, you will see:
+
+Specifies that either Secure Sockets Layer (SSL) 3.0 or Transport Layer Security 1.0 are acceptable for secure communications
+
+I imagine if you override the Default value and set it to Tls12 before connect (or better: Default | Tls11 | Tls12), you might obtain the results you want/expect.
+
+Note that the value of Default in this case is coming from .NET, not from this project, so if this does resolve your problem then it might be worth creating an upstream issue to change the value of Default, though I'm sure it will happen on its own eventually*/
